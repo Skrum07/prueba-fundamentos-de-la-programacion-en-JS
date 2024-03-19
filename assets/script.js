69fb62122a6dfb450e87db6999a957e41cec0100
Tomas Roa <tomas.roa@gmail.com>
2024-03-19 19:24:49 -0300

    se corrigen viculos a la api de super heroe y se agrega el grafico

--- assets/script.js
+++ assets/script.js
@@ -31,47 +31,61 @@ $(document).ready(function(){
     }
 
     function displayHeroInfo(hero){
-        let conteHero = `
-        <div class="container d-flex flex-row justify-content-around">
-        <div class="card" style="width: 18rem;">
-            <img src="${hero.image['url']}" class="card-img-top" alt="${hero.name}">
+        let conteHero =
+        `
+        <h2 style = "text-aligne:center">Este es tu Super Heroe</h2>
+        <div class="card mb-3">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="${hero.image['url']}" class="img-fluid rounded-start" alt="${hero.name}">
+          </div>
+          <div class="col-md-8">
             <div class="card-body">
-              <h5 class="card-title">${hero.name}</h5>
-              <p class="card-text">Connections: ${hero.connections['group.affiliation']}.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+              <h5 class="card-title">Nombre: ${hero.name}</h5>
+              <p class="card-text"><small class="text-body-secondary">Nombre completo: ${hero.biography['full-name']}</small></p>
+              <hr>
+              <p class="card-text"><small class="text-body-secondary">Raza: ${hero.biography['place-of-birth']}</small></p>
+              <hr>
+              <p class="card-text"><small class="text-body-secondary">Alias: ${hero.biography['aliases']}</small></p>
+              <hr>
+              <p class="card-text"><small class="text-body-secondary">Primera aparicion: ${hero.biography['first-appearance']}</small></p>
+              <hr>
+              <p class="card-text"><small class="text-body-secondary">Partners: ${hero.connections['group-afiliation']}</small></p>
+              <hr>
+              <p class="card-text"><small class="text-body-secondary">Peso: ${hero.appearance['weight']}</small></p>
+              <hr>
+
             </div>
           </div>
-          <div id="grafico" style="height: 370px; width: 100%;"></div>
         </div>
-        `;
+      </div>`
 
         $('#conteHero').html(conteHero);
 
         // Grafico Canvasjs
-        // let opciones = {
-        //     animationEnabled: true,
-        //     title: {
-        //         text: `Estadisticas para ${hero.name}`
-        //     },
-        //     data: [{
-        //         type: "doughnut",
-        //         innerRadius: "80%",
-        //         showInLegend: true,
-        //         legendText: "{label}",
-        //         indexLabel: "{label}: #percent%",
-        //         dataPoints: [
-        //             {label: "combat", y: hero.powerstats.combat},
-        //             {label: "intelligence", y: hero.powerstats.intelligence},
-        //             {label: "strength", y: hero.powerstats.strength},
-        //             {label: "speed", y: hero.powerstats.speed},
-        //             {label: "durability", y: hero.powerstats.durability},
-        //             {label: "power", y: hero.powerstats.power},
-        //         ]
-        //     }]
-        // };
-
-        // let chart = new CanvasJS.Chart("grafico", opciones);
-        // chart.render();
-        // $('.canvasjs-chart-credit').html('');
+         let opciones = {
+             animationEnabled: true,
+             title: {
+                 text: `Estadisticas para ${hero.name}`
+             },
+             data: [{
+                 type: "doughnut",
+                 innerRadius: "80%",
+                 showInLegend: true,
+                 legendText: "{label}",
+                 indexLabel: "{label}: #percent%",
+                 dataPoints: [
+                     {label: "intelligence", y: hero.powerstats[1].intelligence},
+                     {label: "strength", y: hero.powerstats[2].strength},
+                     {label: "speed", y: hero.powerstats[3].speed},
+                     {label: "durability", y: hero.powerstats[4].durability},
+                     {label: "power", y: hero.powerstats[5].power},
+                     {label: "combat", y: hero.powerstats[0].combat},
+                 ]
+             }]
+        };
+        
+        $("#grafico").CanvasJSChart(opciones);
+        $('canvasjs-chart-credit').html('')
     }
 });
